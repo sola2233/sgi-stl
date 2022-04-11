29e799fcc49b3adcc3c546eebae8130e10039b74
sola2233 <442785423@qq.com>
2022-04-11 23:12:02 +0800

    add sequence_container comments

--- g++/stl_list.h
+++ g++/stl_list.h
@@ -37,14 +37,16 @@ __STL_BEGIN_NAMESPACE
 #pragma set woff 1174
 #endif
 
+// list 节点结构，显然是一个双向链表
 template <class T>
 struct __list_node {
   typedef void* void_pointer;
+  void_pointer prev;  // 型别为 void*。其实可设为 __list_node<T>*
   void_pointer next;
-  void_pointer prev;
   T data;
 };
 
+// list 的迭代器设计
 template<class T, class Ref, class Ptr>
 struct __list_iterator {
   typedef __list_iterator<T, T&, T*>             iterator;
@@ -59,20 +61,25 @@ struct __list_iterator {
   typedef size_t size_type;
   typedef ptrdiff_t difference_type;
 
-  link_type node;
+  // 迭代器内部要有一个普通指针，指向 list 的节点
+  link_type node; 
 
+  // 构造函数
   __list_iterator(link_type x) : node(x) {}
   __list_iterator() {}
   __list_iterator(const iterator& x) : node(x.node) {}
 
   bool operator==(const self& x) const { return node == x.node; }
   bool operator!=(const self& x) const { return node != x.node; }
+  // 以下对迭代器解引用，取的是节点的数据值
   reference operator*() const { return (*node).data; }
 
 #ifndef __SGI_STL_NO_ARROW_OPERATOR
+  // 以下是迭代器的成员存取（member access）运算子的标准做法
   pointer operator->() const { return &(operator*()); }
 #endif /* __SGI_STL_NO_ARROW_OPERATOR */
 
+  // 对迭代器累加 1，就是前进一个节点，重载前置运算符
   self& operator++() { 
     node = (link_type)((*node).next);
     return *this;
@@ -82,6 +89,8 @@ struct __list_iterator {
     ++*this;
     return tmp;
   }
+
+  // 对迭代器递减 1，就是后退一个节点，重载前置运算符
   self& operator--() { 
     node = (link_type)((*node).prev);
     return *this;
@@ -115,19 +124,21 @@ distance_type(const __list_iterator<T, Ref, Ptr>&) {
 
 #endif /* __STL_CLASS_PARTIAL_SPECIALIZATION */
 
+// list 的结构，默认使用 alloc 作为配置器
 template <class T, class Alloc = alloc>
 class list {
 protected:
   typedef void* void_pointer;
   typedef __list_node<T> list_node;
+  // 专属之空间配置器，每次配置一个节点的大小
   typedef simple_alloc<list_node, Alloc> list_node_allocator;
 public:      
   typedef T value_type;
   typedef value_type* pointer;
-  typedef const value_type* const_pointer;
   typedef value_type& reference;
+  typedef const value_type* const_pointer;
   typedef const value_type& const_reference;
-  typedef list_node* link_type;
+  typedef list_node* link_type; // 定义一个指向节点的指针类型
   typedef size_t size_type;
   typedef ptrdiff_t difference_type;
 
@@ -148,26 +159,31 @@ public:
 #endif /* __STL_CLASS_PARTIAL_SPECIALIZATION */
 
 protected:
+  // 配置一个节点并传回
   link_type get_node() { return list_node_allocator::allocate(); }
+  // 释放一个节点
   void put_node(link_type p) { list_node_allocator::deallocate(p); }
 
+  // 产生（配置并构造）一个节点，带有元素值
   link_type create_node(const T& x) {
     link_type p = get_node();
     __STL_TRY {
-      construct(&p->data, x);
+      construct(&p->data, x); // 全局函数，构造/析构基本工具
     }
     __STL_UNWIND(put_node(p));
     return p;
   }
+  // 销毁（析构并释放）一个节点
   void destroy_node(link_type p) {
-    destroy(&p->data);
+    destroy(&p->data);  // 全局函数，构造/析构基本工具
     put_node(p);
   }
 
 protected:
+  // 产生一个空链表
   void empty_initialize() { 
-    node = get_node();
-    node->next = node;
+    node = get_node();  // 配置一个节点空间，令 node 指向它
+    node->next = node;  // 令 node 头尾都指向自己，不设元素值
     node->prev = node;
   }
 
@@ -206,37 +222,47 @@ protected:
 #endif /* __STL_MEMBER_TEMPLATES */
 
 protected:
-  link_type node;
+  // 让指针 node 指向可以置于尾端的一个空白节点，node 便能符合 STL
+  // 对于 “前闭后开” 区间的要求，成为 last 迭代器
+  link_type node;  // 只要一个指针，便可表示整个环状双向链表
 
 public:
+  // 默认构造函数，产生一个空链表
   list() { empty_initialize(); }
 
+  // 利用 node 节点，便能实现 begin 和 end
   iterator begin() { return (link_type)((*node).next); }
-  const_iterator begin() const { return (link_type)((*node).next); }
   iterator end() { return node; }
+  const_iterator begin() const { return (link_type)((*node).next); }
   const_iterator end() const { return node; }
   reverse_iterator rbegin() { return reverse_iterator(end()); }
+  reverse_iterator rend() { return reverse_iterator(begin()); }
   const_reverse_iterator rbegin() const { 
     return const_reverse_iterator(end()); 
   }
-  reverse_iterator rend() { return reverse_iterator(begin()); }
   const_reverse_iterator rend() const { 
     return const_reverse_iterator(begin());
   } 
   bool empty() const { return node->next == node; }
   size_type size() const {
     size_type result = 0;
-    distance(begin(), end(), result);
+    distance(begin(), end(), result); // 全局函数，第三章
     return result;
   }
   size_type max_size() const { return size_type(-1); }
+  // 取头节点的内容（元素值）
   reference front() { return *begin(); }
-  const_reference front() const { return *begin(); }
+  // 取尾节点的内容（元素值）
   reference back() { return *(--end()); }
+  const_reference front() const { return *begin(); }
   const_reference back() const { return *(--end()); }
   void swap(list<T, Alloc>& x) { __STD::swap(node, x.node); }
+
+  // 在迭代器 position 所指位置插入一个节点，内容为 x
   iterator insert(iterator position, const T& x) {
+    // 产生一个节点，内容为 x
     link_type tmp = create_node(x);
+    // 调整双向指针，使 tmp 插入进去
     tmp->next = position.node;
     tmp->prev = position.node->prev;
     (link_type(position.node->prev))->next = tmp;
@@ -260,8 +286,12 @@ public:
     insert(pos, (size_type)n, x);
   }
 
+  // 插入一个节点，作为头节点
   void push_front(const T& x) { insert(begin(), x); }
+  // 插入一个节点，作为尾节点
   void push_back(const T& x) { insert(end(), x); }
+
+  // 移除迭代器 position 所指节点
   iterator erase(iterator position) {
     link_type next_node = link_type(position.node->next);
     link_type prev_node = link_type(position.node->prev);
@@ -275,7 +305,9 @@ public:
   void resize(size_type new_size) { resize(new_size, T()); }
   void clear();
 
+  // 移除头节点
   void pop_front() { erase(begin()); }
+  // 移除尾节点
   void pop_back() { 
     iterator tmp = end();
     erase(--tmp);
@@ -307,8 +339,10 @@ public:
   list<T, Alloc>& operator=(const list<T, Alloc>& x);
 
 protected:
+  // 将 [first, last) 内的所有元素移动到 position 之前
   void transfer(iterator position, iterator first, iterator last) {
     if (position != last) {
+      // 具体过程见 p139
       (*(link_type((*last.node).prev))).next = position.node;
       (*(link_type((*first.node).prev))).next = last.node;
       (*(link_type((*position.node).prev))).next = first.node;  
@@ -320,20 +354,28 @@ protected:
   }
 
 public:
+  // 将 x 接合于 position 所指位置之前。x 必须不同于 *this
   void splice(iterator position, list& x) {
     if (!x.empty()) 
       transfer(position, x.begin(), x.end());
   }
+
+  // 将 i 所指元素接合于 position 所指位置之前。position 和 i 可指向同一个 list
   void splice(iterator position, list&, iterator i) {
     iterator j = i;
     ++j;
     if (position == i || position == j) return;
     transfer(position, i, j);
   }
+
+  // 将 [first, last) 内的所有元素接合于 position 所指位置之前
+  // position 和 [first, last) 可指向同一个 list
+  // 但 position 不能位于 [first, last) 之内
   void splice(iterator position, list&, iterator first, iterator last) {
     if (first != last) 
       transfer(position, first, last);
   }
+
   void remove(const T& value);
   void unique();
   void merge(list& x);
@@ -429,15 +471,17 @@ void list<T, Alloc>::resize(size_type new_size, const T& x)
     insert(end(), new_size - len, x);
 }
 
+// 清除所有节点（整个链表）
 template <class T, class Alloc> 
 void list<T, Alloc>::clear()
 {
   link_type cur = (link_type) node->next;
-  while (cur != node) {
+  while (cur != node) {   // 遍历每一个节点
     link_type tmp = cur;
-    cur = (link_type) cur->next;
+    cur = (link_type) cur->next;  // 销毁（析构并释放）一个节点
     destroy_node(tmp);
   }
+  // 恢复 node 原始状态
   node->next = node;
   node->prev = node;
 }
@@ -458,79 +502,110 @@ list<T, Alloc>& list<T, Alloc>::operator=(const list<T, Alloc>& x) {
   return *this;
 }
 
+// 将数值为 value 之所有元素移除
 template <class T, class Alloc>
 void list<T, Alloc>::remove(const T& value) {
   iterator first = begin();
   iterator last = end();
-  while (first != last) {
+  while (first != last) {   // 遍历每一个节点
     iterator next = first;
     ++next;
-    if (*first == value) erase(first);
+    if (*first == value) erase(first);  // 找到就移除
     first = next;
   }
 }
 
+// 移除数值相同的连续元素。注意，只有 “连续而相同的元素” 才会被移除剩一个
 template <class T, class Alloc>
 void list<T, Alloc>::unique() {
   iterator first = begin();
   iterator last = end();
-  if (first == last) return;
+  if (first == last) return;  // 空链表，什么都不必做
   iterator next = first;
-  while (++next != last) {
-    if (*first == *next)
-      erase(next);
+  while (++next != last) {    // 遍历每一个节点
+    if (*first == *next)      // 如果在此区段中有相同的元素
+      erase(next);            // 移除之
     else
-      first = next;
-    next = first;
+      first = next;           // 调整指针
+    next = first;             // 修正区段范围
   }
 }
 
+// merge() 将 x 合并到 *this 身上。两个 lists 的内容都必须先经过递增排序
 template <class T, class Alloc>
 void list<T, Alloc>::merge(list<T, Alloc>& x) {
   iterator first1 = begin();
   iterator last1 = end();
   iterator first2 = x.begin();
   iterator last2 = x.end();
+
+  // 注意：前提是，两个 lists 都已经过递增排序
   while (first1 != last1 && first2 != last2)
     if (*first2 < *first1) {
       iterator next = first2;
+      // 把 first2 所指的节点移动到 first1 前面
+      // 只有小于才移动，所以是稳定的
       transfer(first1, first2, ++next);
       first2 = next;
     }
     else
       ++first1;
+  // 如果 x 还有剩下的节点，将 [first2, last2) 内的节点移动到 last1 之前
   if (first2 != last2) transfer(last1, first2, last2);
 }
 
+// reverse() 将 *this 的内容逆向重置
 template <class T, class Alloc>
 void list<T, Alloc>::reverse() {
+  // 以下判断，如果是空链表，或仅有一个元素，就不进行任何操作
+  // 使用 size() == 0 || size() == 1 来判断，虽然也可以，但是比较慢
   if (node->next == node || link_type(node->next)->next == node) return;
   iterator first = begin();
-  ++first;
+  ++first;  // first 先调整到第二个节点
   while (first != end()) {
     iterator old = first;
     ++first;
+    // 每次将 old 指向的节点移动到 begin() 之前
     transfer(begin(), old, first);
   }
 }    
 
+// list 不能使用 STL 算法 sort()，必须使用自己的 sort() member function，
+// 因为 STL 算法 sort() 只接受 RandomAccessIterator
+// 本函数采用 merge sort，是 merge sort 的迭代版本
 template <class T, class Alloc>
 void list<T, Alloc>::sort() {
+  // 以下判断，如果是空链表，或仅有一个元素，就不进行任何操作
+  // 使用 size() == 0 || size() == 1 来判断，虽然也可以，但是比较慢  
   if (node->next == node || link_type(node->next)->next == node) return;
+
+  // 一些新的 list，作为中介数据存放区
   list<T, Alloc> carry;
+  // 64 个槽位，最多存放 2^63 大小的数据，完全够用
   list<T, Alloc> counter[64];
+  // 用表示最后一个存放数据的槽位后面那个空槽位的下标
   int fill = 0;
   while (!empty()) {
+    // 每次从 list 中取出一个节点（begin()）放到 carry 中
     carry.splice(carry.begin(), *this, begin());
     int i = 0;
+    // 结束条件，遇到了下一个为空的槽位或前面的旧的空槽位
     while(i < fill && !counter[i].empty()) {
+      // merge 把两个有序链表合并为一个
       counter[i].merge(carry);
+      // 交换到 carry 里，往后面槽位里的链表合并
       carry.swap(counter[i++]);
     }
-    carry.swap(counter[i]);         
+    // 结束循环后再把 carry 里的数据转移到 counter 中
+    // 如果是 i < fill 条件结束的，就放到 counter[fill] 中
+    // 如果是 !counter[i].empty() 条件结束的，就放到前面旧的空槽位中，等待下次合并
+    carry.swap(counter[i]);
+    // 如果是 i < fill 条件结束的，此时 counter[fill] 中已经存放了数据
+    // 所以 fill 要自增，找到下一个空槽位
     if (i == fill) ++fill;
   } 
 
+  // 合并 counter 所有槽位的链表
   for (int i = 1; i < fill; ++i) counter[i].merge(counter[i-1]);
   swap(counter[fill-1]);
 }
--- g++/stl_vector.h
+++ g++/stl_vector.h
@@ -37,15 +37,17 @@ __STL_BEGIN_NAMESPACE
 #pragma set woff 1174
 #endif
 
+// alloc 是 SGI STL 的空间配置器
 template <class T, class Alloc = alloc>
 class vector {
 public:
+  // vector 的内嵌型别定义
   typedef T value_type;
   typedef value_type* pointer;
-  typedef const value_type* const_pointer;
-  typedef value_type* iterator;
-  typedef const value_type* const_iterator;
+  typedef value_type* iterator;   // vector 的迭代器是普通指针
   typedef value_type& reference;
+  typedef const value_type* const_pointer;
+  typedef const value_type* const_iterator;
   typedef const value_type& const_reference;
   typedef size_t size_type;
   typedef ptrdiff_t difference_type;
@@ -60,46 +62,57 @@ public:
           reverse_iterator;
 #endif /* __STL_CLASS_PARTIAL_SPECIALIZATION */
 protected:
+  // 以下，simple_alloc 是 SGI STL 的空间配置器，见 2.2.4 节
   typedef simple_alloc<value_type, Alloc> data_allocator;
-  iterator start;
-  iterator finish;
-  iterator end_of_storage;
+  iterator start;           // 表示目前使用空间的头
+  iterator finish;          // 表示目前使用空间的尾
+  iterator end_of_storage;  // 表示目前可用空间的尾
+
   void insert_aux(iterator position, const T& x);
+  // vector 的 member function，调用空间配置器的 deallocate
   void deallocate() {
-    if (start) data_allocator::deallocate(start, end_of_storage - start);
+    if (start) 
+      // 大于 128KB 的空间会调用一级配置器
+      // 小于等于的会调用 alloc 的 deallocate，把空闲节点放回空闲链表的合适位置
+      data_allocator::deallocate(start, end_of_storage - start);
   }
 
+  // 填充并予以初始化
   void fill_initialize(size_type n, const T& value) {
+    // 调用 allocate_and_fill（见本头文件）配置空间并初始化
     start = allocate_and_fill(n, value);
     finish = start + n;
     end_of_storage = finish;
   }
 public:
+  // 容器中的迭代器
   iterator begin() { return start; }
-  const_iterator begin() const { return start; }
   iterator end() { return finish; }
+  const_iterator begin() const { return start; }
   const_iterator end() const { return finish; }
   reverse_iterator rbegin() { return reverse_iterator(end()); }
+  reverse_iterator rend() { return reverse_iterator(begin()); }
   const_reverse_iterator rbegin() const { 
     return const_reverse_iterator(end()); 
   }
-  reverse_iterator rend() { return reverse_iterator(begin()); }
   const_reverse_iterator rend() const { 
     return const_reverse_iterator(begin()); 
   }
   size_type size() const { return size_type(end() - begin()); }
+  // max_size() 计算方法：把一个二进制位全 1 的数转换为无符号整数，除以单个元素的大小
   size_type max_size() const { return size_type(-1) / sizeof(T); }
   size_type capacity() const { return size_type(end_of_storage - begin()); }
   bool empty() const { return begin() == end(); }
   reference operator[](size_type n) { return *(begin() + n); }
   const_reference operator[](size_type n) const { return *(begin() + n); }
 
+  // 构造函数
   vector() : start(0), finish(0), end_of_storage(0) {}
-  vector(size_type n, const T& value) { fill_initialize(n, value); }
+  vector(size_type n, const T& value) { fill_initialize(n, value); } // 允许指定 vector 大小 n 和初值 value
   vector(int n, const T& value) { fill_initialize(n, value); }
   vector(long n, const T& value) { fill_initialize(n, value); }
   explicit vector(size_type n) { fill_initialize(n, T()); }
-
+  // 拷贝构造函数
   vector(const vector<T, Alloc>& x) {
     start = allocate_and_copy(x.end() - x.begin(), x.begin(), x.end());
     finish = start + (x.end() - x.begin());
@@ -121,9 +134,10 @@ public:
     end_of_storage = finish;
   }
 #endif /* __STL_MEMBER_TEMPLATES */
+  // 析构函数
   ~vector() { 
-    destroy(start, finish);
-    deallocate();
+    destroy(start, finish); // 全局函数，见 2.2.3 节
+    deallocate();           // 这是vector 的一个 member function
   }
   vector<T, Alloc>& operator=(const vector<T, Alloc>& x);
   void reserve(size_type n) {
@@ -137,23 +151,27 @@ public:
       end_of_storage = start + n;
     }
   }
-  reference front() { return *begin(); }
+
+  reference front() { return *begin(); }    // 第一个元素
+  reference back() { return *(end() - 1); } // 最后一个元素
   const_reference front() const { return *begin(); }
-  reference back() { return *(end() - 1); }
   const_reference back() const { return *(end() - 1); }
+  // 尾部插入一个元素
   void push_back(const T& x) {
-    if (finish != end_of_storage) {
-      construct(finish, x);
-      ++finish;
+    if (finish != end_of_storage) { // 还有备用空间
+      construct(finish, x);         // 全局函数，见 2.2.3 节
+      ++finish;                     // 调整水位高度
     }
-    else
-      insert_aux(end(), x);
+    else                            // 已无备用空间
+      insert_aux(end(), x); // 这是 vector 的一个 member function
   }
+
   void swap(vector<T, Alloc>& x) {
     __STD::swap(start, x.start);
     __STD::swap(finish, x.finish);
     __STD::swap(end_of_storage, x.end_of_storage);
   }
+
   iterator insert(iterator position, const T& x) {
     size_type n = position - begin();
     if (finish != end_of_storage && position == end()) {
@@ -164,6 +182,7 @@ public:
       insert_aux(position, x);
     return begin() + n;
   }
+
   iterator insert(iterator position) { return insert(position, T()); }
 #ifdef __STL_MEMBER_TEMPLATES
   template <class InputIterator>
@@ -183,23 +202,29 @@ public:
     insert(pos, (size_type) n, x);
   }
 
+  // 将最尾端元素取出，并调整大小
   void pop_back() {
-    --finish;
-    destroy(finish);
+    --finish;         // 将尾端标记往前移一格，表示放弃尾端元素
+    destroy(finish);  // destroy 是全局函数，见 2.2.3 节
   }
+
+  // 清除某位置上的元素
   iterator erase(iterator position) {
     if (position + 1 != end())
-      copy(position + 1, finish, position);
+      copy(position + 1, finish, position); // 后续元素往前移动，copy 是全局函数，第 6 章
     --finish;
-    destroy(finish);
+    destroy(finish);  // 全局函数，见 2.2.3 节
     return position;
   }
+
+  // 清除 [first, last) 中的所有元素
   iterator erase(iterator first, iterator last) {
-    iterator i = copy(last, finish, first);
-    destroy(i, finish);
+    iterator i = copy(last, finish, first); // 后续元素往前移动，copy 是全局函数，第 6 章
+    destroy(i, finish); // destroy 是全局函数，第 2 章
     finish = finish - (last - first);
     return first;
   }
+
   void resize(size_type new_size, const T& x) {
     if (new_size < size()) 
       erase(begin() + new_size, end());
@@ -207,13 +232,16 @@ public:
       insert(end(), new_size - size(), x);
   }
   void resize(size_type new_size) { resize(new_size, T()); }
+
+  // 删除容器所有元素
   void clear() { erase(begin(), end()); }
 
 protected:
+  // 配置空间然后填充
   iterator allocate_and_fill(size_type n, const T& x) {
-    iterator result = data_allocator::allocate(n);
+    iterator result = data_allocator::allocate(n);  // 配置 n 个元素的空间
     __STL_TRY {
-      uninitialized_fill_n(result, n, x);
+      uninitialized_fill_n(result, n, x); // 全局函数，见 2.3 节
       return result;
     }
     __STL_UNWIND(data_allocator::deallocate(result, n));
@@ -321,80 +349,113 @@ vector<T, Alloc>& vector<T, Alloc>::operator=(const vector<T, Alloc>& x) {
 
 template <class T, class Alloc>
 void vector<T, Alloc>::insert_aux(iterator position, const T& x) {
-  if (finish != end_of_storage) {
+  if (finish != end_of_storage) {   // 还有备用空间
+    // 在备用空间起始处构造一个元素，并以 vector 最后一个元素值为其初值
     construct(finish, *(finish - 1));
+    // 调整水位
     ++finish;
     T x_copy = x;
     copy_backward(position, finish - 2, finish - 1);
     *position = x_copy;
   }
-  else {
+  else {    // 已无备用空间
     const size_type old_size = size();
     const size_type len = old_size != 0 ? 2 * old_size : 1;
-    iterator new_start = data_allocator::allocate(len);
+    // 以上配置原则：如果原大小为 0，则配置 1（个元素大小）；
+    // 如果原大小不为 0，则配置原大小的两倍，
+    // 前半段用来放置原数据，后半段准备用来放置新数据
+
+    iterator new_start = data_allocator::allocate(len); // 实际配置
     iterator new_finish = new_start;
     __STL_TRY {
+      // 将原 vector 的内容拷贝到新 vector
       new_finish = uninitialized_copy(start, position, new_start);
+      // 为新元素设定初值 x
       construct(new_finish, x);
+      // 调整水位
       ++new_finish;
+      // 将安差点的原内容也拷贝过来（提示：本函数也可能被 insert(p, x) 调用，除了被 push_back 扩充空间时调用外）
       new_finish = uninitialized_copy(position, finish, new_finish);
     }
 
 #       ifdef  __STL_USE_EXCEPTIONS 
+    // 回滚，构造不成功时
     catch(...) {
       destroy(new_start, new_finish); 
       data_allocator::deallocate(new_start, len);
       throw;
     }
 #       endif /* __STL_USE_EXCEPTIONS */
+    // 析构并释放原 vector
     destroy(begin(), end());
-    deallocate();
+    deallocate(); // 调用 vector 内的 member function，成员函数负责调用 simple_allocate 的d eallocate
+
+    // 调整迭代器，指向新 vector
     start = new_start;
     finish = new_finish;
     end_of_storage = new_start + len;
   }
 }
 
+// 从 position 开始，插入 n 个元素，元素初值为 x
 template <class T, class Alloc>
 void vector<T, Alloc>::insert(iterator position, size_type n, const T& x) {
-  if (n != 0) {
+  if (n != 0) { // 当 n != 0 才进行以下所有操作
     if (size_type(end_of_storage - finish) >= n) {
+      // 当备用空间大于等于 “新增元素个数”
       T x_copy = x;
+      // 以下计算插入点之后的现有元素个数
       const size_type elems_after = finish - position;
       iterator old_finish = finish;
-      if (elems_after > n) {
+      if (elems_after > n) {  // “插入点之后的现有元素个数” 大于 “新增元素个数”
+        // 将插入点后的倒数 n 个元素复制到 finish 开始的备用空间
         uninitialized_copy(finish - n, finish, finish);
+        // 将 vector 尾端标记后移
         finish += n;
+        // 将插入点后剩余的 elems_after - n 个元素复制到后面
         copy_backward(position, old_finish - n, old_finish);
         fill(position, position + n, x_copy);
       }
-      else {
+      else {  // “插入点之后的现有元素个数” 小于等于 “新增元素个数”
+        // 在 vector 尾部填充 n - elems_after 个元素，初值为 x
         uninitialized_fill_n(finish, n - elems_after, x_copy);
+        // 将 vector 尾端标记后移
         finish += n - elems_after;
+        // 将插入点后的所有元素复制到 vector 尾部
         uninitialized_copy(position, old_finish, finish);
+        // 将 vector 尾端标记后移
         finish += elems_after;
+        // 从插入点开始填入新值
         fill(position, old_finish, x_copy);
       }
     }
-    else {
+    else {  // 备用空间小于 “新增元素个数”（那就必须配置额外的内存）
+      // 首先决定新长度：旧长度的两倍，或旧长度+新增元素个数（如果新增元素个数大于旧长度）
       const size_type old_size = size();        
       const size_type len = old_size + max(old_size, n);
+      // 以下配置新的 vector 空间
       iterator new_start = data_allocator::allocate(len);
       iterator new_finish = new_start;
       __STL_TRY {
+        // 以下首先将旧 vector 的插入点之前的元素复制到新空间
         new_finish = uninitialized_copy(start, position, new_start);
+        // 以下再将新增元素（初值皆为 x）填入新空间
         new_finish = uninitialized_fill_n(new_finish, n, x);
+        // 以下再将旧 vector 的插入点之后的元素复制到新空间
         new_finish = uninitialized_copy(position, finish, new_finish);
       }
 #         ifdef  __STL_USE_EXCEPTIONS 
       catch(...) {
+        // 如有异常发生，实现回滚
         destroy(new_start, new_finish);
         data_allocator::deallocate(new_start, len);
         throw;
       }
 #         endif /* __STL_USE_EXCEPTIONS */
+      // 以下清除并释放旧的 vector
       destroy(start, finish);
       deallocate();
+      // 以下调整水位标记
       start = new_start;
       finish = new_finish;
       end_of_storage = new_start + len;
